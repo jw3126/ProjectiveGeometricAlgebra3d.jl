029553ee3f69c39f51bfb7e3a347899e9093d6eb
Documenter.jl <documenter@juliadocs.github.io>
2021-04-21 10:15:56 +0000

    build based on d2db589

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProjectiveGeometricAlgebra3d","category":"page"},{"location":"#ProjectiveGeometricAlgebra3d","page":"Home","title":"ProjectiveGeometricAlgebra3d","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProjectiveGeometricAlgebra3d.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProjectiveGeometricAlgebra3d]","category":"page"}]
+}
